b0d8b8ceebc54b120fd2c02ef42cd6d59dd2a701
Cherry <cherry@sparklet.org>
2024-07-21 14:12:58 -0400

    use stacks for categories (no sidebar yet)

--- src/emoji_ls.rs
+++
@@ -1,5 +0,0 @@
-use super::*;
-
-pub fn list_emojis() -> impl Iterator<Item = &'static Emoji> {
-    emojis::iter().filter(|e| e.unicode_version() < emojis::UnicodeVersion::new(13, 0))
-}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use emojis::Emoji;
-use gtk::{glib, Application, ApplicationWindow, Grid};
+use emojis::{Emoji, Group};
+use gtk::{glib, Application, ApplicationWindow, Grid, Stack};
 use gtk::{prelude::*, Button};
-
-mod emoji_ls;
 
 const APP_ID: &str = "org.sparklet.windot";
 const EMOJIS_PER_ROW: i32 = 10;
+
+const GROUPS: &[Group] = &[
+    Group::SmileysAndEmotion,
+    Group::PeopleAndBody,
+    Group::AnimalsAndNature,
+    Group::Activities,
+    Group::FoodAndDrink,
+    Group::Objects,
+    Group::TravelAndPlaces,
+    Group::Symbols,
+    Group::Flags,
+];
 
 fn main() -> glib::ExitCode {
     let app = Application::builder().application_id(APP_ID).build();
@@ -14,6 +24,26 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
+    let stack = Stack::new();
+
+    for group in GROUPS {
+        let grid = build_grid(*group);
+        let name = format!("{:?}", group);
+        stack.add_titled(&grid, Some(&name), &name);
+    }
+
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Select an emoji.")
+        .child(&stack)
+        .build();
+
+    // Present window
+    window.present();
+}
+
+fn build_grid(group: Group) -> Grid {
     let grid = Grid::builder()
         .column_spacing(10)
         .margin_top(10)
@@ -27,7 +57,7 @@ fn build_ui(app: &Application) {
     let mut row = 0;
     let mut col = 0;
 
-    for emoji in emoji_ls::list_emojis() {
+    for emoji in group.emojis().take(10) {
         let button = Button::builder().label(emoji.to_string()).build();
 
         button.connect_clicked(|button| {
@@ -43,13 +73,5 @@ fn build_ui(app: &Application) {
         }
     }
 
-    // Create a window
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("Select an emoji.")
-        .child(&grid)
-        .build();
-
-    // Present window
-    window.present();
+    grid
 }

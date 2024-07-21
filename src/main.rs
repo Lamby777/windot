4babb14cd549fdd97ee365ce216eea5383655446
Cherry <cherry@sparklet.org>
2024-07-21 13:38:29 -0400

    draw grid of piggies

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use gtk::{prelude::*, Button};
 mod emoji_ls;
 
 const APP_ID: &str = "org.sparklet.windot";
+const EMOJIS_PER_ROW: i32 = 10;
 
 fn main() -> glib::ExitCode {
     let app = Application::builder().application_id(APP_ID).build();
@@ -22,14 +23,26 @@ fn build_ui(app: &Application) {
         .column_homogeneous(true)
         .build();
 
-    for i in 0..10 {
-        let button = Button::builder().label(i.to_string()).build();
+    // just piggies for now
+    let emojis = ['🐷'; 21];
+
+    let mut row = 0;
+    let mut col = 0;
+
+    for emoji in emojis.iter() {
+        let button = Button::builder().label(emoji.to_string()).build();
 
         button.connect_clicked(|button| {
-            button.set_label("Hello World!");
+            println!("Button clicked: {}", button.label().unwrap());
         });
+        grid.attach(&button, col, row, 1, 1);
 
-        grid.attach(&button, i, 0, 1, 1);
+        col += 1;
+
+        if col == EMOJIS_PER_ROW {
+            col = 0;
+            row += 1;
+        }
     }
 
     // Create a window

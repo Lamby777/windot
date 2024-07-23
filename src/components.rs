eed3b3f987d0518758a3d9818d2a4a1a22a9571d
Cherry <cherry@sparklet.org>
2024-07-23 02:37:47 -0400

    split components code into another file

---
+++ src/components.rs
@@ -0,0 +1,70 @@
+use super::*;
+
+pub fn build_search() -> gtk::Box {
+    let stack = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+
+    let grid = build_grid(all_emojis());
+
+    let searchbox = SearchEntry::builder().build();
+
+    stack.append(&searchbox);
+    stack.append(&grid);
+
+    searchbox.connect_search_changed(move |sb| {
+        let parent: gtk::Box = unsafe { sb.parent().unwrap().unsafe_cast() };
+        parent.remove(&parent.last_child().unwrap());
+
+        parent.append(&build_grid(all_emojis().filter(|e| {
+            e.shortcodes().any(|sc| sc.contains(&sb.text().to_string()))
+        })));
+    });
+
+    stack
+}
+
+pub fn build_grid(
+    emojis: impl Iterator<Item = &'static Emoji>,
+) -> ScrolledWindow {
+    let grid = Grid::builder()
+        .column_spacing(10)
+        .margin_top(10)
+        .margin_bottom(10)
+        .margin_start(10)
+        .margin_end(10)
+        .row_homogeneous(true)
+        .column_homogeneous(true)
+        .build();
+
+    let mut row = 0;
+    let mut col = 0;
+
+    // for emoji in emojis.take(EMOJIS_PER_PAGE) {
+    for emoji in emojis {
+        let button = Button::builder().label(emoji.to_string()).build();
+
+        button.connect_clicked(|button| {
+            let emoji = button.label().unwrap();
+            println!("Button clicked: {}", emoji);
+            cli_clipboard::set_contents(emoji.to_string()).unwrap();
+            WINDOW.get().unwrap().0.close();
+        });
+        grid.attach(&button, col, row, 1, 1);
+
+        col += 1;
+
+        if col == EMOJIS_PER_ROW {
+            col = 0;
+            row += 1;
+        }
+    }
+
+    ScrolledWindow::builder()
+        .hscrollbar_policy(gtk::PolicyType::Never)
+        .vscrollbar_policy(gtk::PolicyType::Automatic)
+        .width_request(500)
+        .height_request(400)
+        .child(&grid)
+        .build()
+}
